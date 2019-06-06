15d1fd8ae7d4be30a85fb58c3132839dd46c99a3
liangxiaojun <fatalxiao@163.com>
2019-06-06 10:34:01 +0800

    update PackageMaxValue

--- DynamicProgramming/PackageMaxValue/PackageMaxValue.js
+++ DynamicProgramming/PackageMaxValue/PackageMaxValue.js
@@ -1,35 +1,41 @@
+/**
+ * 背包问题
+ * @param items
+ * @param size
+ * @returns {Array}
+ */
 function packageMaxValue(items, size) {
 
-    let bagMatrix = [];
+    const cache = [];
 
     for (let i = 0; i <= size; i++) {
 
-        bagMatrix[i] = [];
+        cache[i] = [];
 
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < items.length; j++) {
 
             // 背包的容量为0，那么一个东西也装不下，此时的值肯定也是为0
             if (i === 0) {
-                bagMatrix[i][j] = 0;
+                cache[i][j] = 0;
                 continue;
             }
 
             // 背包的容量小于物品j的重量，那么就没有上述情况a了
             if (i < items[j].wight) {
-                bagMatrix[i][j] = bagMatrix[i][j - 1] || 0;
+                cache[i][j] = cache[i][j - 1] || 0;
                 continue;
             }
 
-            bagMatrix[i][j] = Math.max(
-                (bagMatrix[i - items[j].wight][j - 1] || 0) + items[j].value,
-                bagMatrix[i][j - 1] || 0
+            cache[i][j] = Math.max(
+                (cache[i - items[j].wight][j - 1] || 0) + items[j].value,
+                cache[i][j - 1] || 0
             );
 
         }
 
     }
 
-    return bagMatrix;
+    return cache;
 
 }
 
