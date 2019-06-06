f72821cc8b25cc0d622c7ec91bb0562bbba5b58c
liangxiaojun <fatalxiao@163.com>
2019-06-06 09:59:55 +0800

    update MinMoneyCount

--- DynamicProgramming/MinMoneyCount.js
+++ DynamicProgramming/MinMoneyCount.js
@@ -1,19 +1,31 @@
-function MinMoneyCount(set, value) {
+/**
+ * 用最少的钞票
+ * @param moneySet
+ * @param value
+ * @returns {*}
+ * @constructor
+ */
+function MinMoneyCount(moneySet, value) {
 
     const cache = [];
     let count;
 
     cache[0] = 0;
+
+    // 遍历所有小于等于满足金额的值
     for (let restValue = 1; restValue <= value; restValue++) {
 
         count = Number.POSITIVE_INFINITY;
 
-        for (let cost of set) {
-            if (restValue >= cost) {
-                count = Math.min(count, cache[restValue - cost] + 1);
-            }
-        }
+        // 遍历所有币种
+        for (let money of moneySet) {
 
+            // 当前剩余金额大于等于当前币种的话，计算最小值
+            if (restValue >= money) {
+                count = Math.min(count, cache[restValue - money] + 1);
+            }
+
+        }
         cache[restValue] = count;
 
         console.log(`min count of ${restValue} = ${count}`);
