63daa51ec24bd90ab030122cd1df7a89cfc8ed6a
liangxiaojun <fatalxiao@163.com>
2019-06-06 09:42:19 +0800

    update LongestIncreaseSequenceRecursion

--- DynamicProgramming/LongestIncreaseSequenceRecursion.js
+++ DynamicProgramming/LongestIncreaseSequenceRecursion.js
@@ -1,16 +1,26 @@
-function LongestIncreaseSequenceRecursion(list) {
+/**
+ * 最长上升序列递归实现
+ * @param list
+ * @returns {number}
+ * @constructor
+ */
+function LISRecursion(list) {
 
     const len = list.length;
 
+    // 如果截取的序列长度为1，则直接返回1
     if (len === 1) {
         return 1;
     }
 
     return Math.max(
-        LongestIncreaseSequenceRecursion(list.slice(1)) + (list[1] > list[0] ? 1 : 0),
-        LongestIncreaseSequenceRecursion(list.slice(0, len - 1)) + (list[len - 1] > list[len - 2] ? 1 : 0)
+        // 掐头，并且若舍去的数小于剩余的第一个数，则需加一
+        LISRecursion(list.slice(1)) + (list[1] > list[0] ? 1 : 0),
+
+        // 去尾，并且若舍去的数大于剩余的最后一个数，则需加一
+        LISRecursion(list.slice(0, len - 1)) + (list[len - 1] > list[len - 2] ? 1 : 0)
     );
 
 }
 
-console.log('Result: ', LongestIncreaseSequenceRecursion([1, 5, 3, 4, 6, 9, 7, 8]));
+console.log('Result: ', LISRecursion([1, 5, 3, 4, 6, 9, 7, 8]));
