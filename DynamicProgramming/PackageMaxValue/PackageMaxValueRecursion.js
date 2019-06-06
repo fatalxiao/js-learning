9c70b586dda192be4cd39ada28799018b86b2e3d
liangxiaojun <fatalxiao@163.com>
2019-06-06 14:34:41 +0800

    update PackageMaxValueRecursion

--- DynamicProgramming/PackageMaxValue/PackageMaxValueRecursion.js
+++ DynamicProgramming/PackageMaxValue/PackageMaxValueRecursion.js
@@ -6,22 +6,28 @@
  */
 function PackageMaxValueRecursion(restItems, restSize) {
 
+    // 过滤掉只有 weight 大于当前剩余容量的物品
     const matchedItems = restItems.filter(item => item.weight <= restSize);
 
+    // 如果无合法物品，返回价值0
     if (matchedItems.length < 1) {
         return 0;
     }
 
+    // （此部分也可省略）如果只剩一个合法物品时直接返回其价值
     if (matchedItems.length === 1) {
         return matchedItems[0].value;
     }
 
+    // 递归使用任意一件物品，计算所有可能情况下的最大价值
     const result = Math.max(
         ...matchedItems.map((item, index) => {
 
+            // 排除当前物品，剩下的作为下一次计算的剩余物品集合
             const nextRestItems = matchedItems.slice();
             nextRestItems.splice(index, 1);
 
+            // 递归调用
             return PackageMaxValueRecursion(nextRestItems, restSize - item.weight) + item.value;
 
         })
