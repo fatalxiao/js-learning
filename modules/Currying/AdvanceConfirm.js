3316441544e89e400b8a1d254475381577b30e11
liangxiaojun <fatalxiao@163.com>
2019-09-04 21:06:19 +0800

    update currying AdvanceConfirm

---
+++ modules/Currying/AdvanceConfirm.js
@@ -0,0 +1,20 @@
+const on = (element, event, handler) => {
+    if (document.addEventListener) {
+        element.addEventListener(event, handler, false);
+    } else {
+        element.attachEvent('on' + event, handler);
+    }
+};
+
+on(document, 'scroll', e => console.log(e));
+
+const curryingOn = () => {
+    if (document.addEventListener) {
+        return (element, event, handler) => element.addEventListener(event, handler, false);
+    } else {
+        return (element, event, handler) => element.attachEvent('on' + event, handler);
+    }
+};
+
+const eventOn = curryingOn();
+eventOn(document, 'scroll', e => console.log(e));
