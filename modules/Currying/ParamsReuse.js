e531897b047c5d626b5b5ae83541dffac36e7152
liangxiaojun <fatalxiao@163.com>
2019-09-05 08:43:02 +0800

    update currying ParamsReuse

--- modules/Currying/ParamsReuse.js
+++ modules/Currying/ParamsReuse.js
@@ -4,3 +4,8 @@ const curryingSum = sum.bind(null, 1, 2);
 curryingSum(3); // 6
 curryingSum(4); // 7
 curryingSum(5); // 8
+
+// bind
+Function.prototype.bind = function (context, ...args) {
+    return (...rest) => this.call(context, ...args, ...rest);
+};
