1f6aa2e3236c187ec2fa0ef4b5fd3349effb0fde
liangxiaojun <fatalxiao@163.com>
2019-09-05 09:03:42 +0800

    update currying Abstract2

--- modules/Currying/Abstract2.js
+++ modules/Currying/Abstract2.js
@@ -1,8 +1,16 @@
+/**
+ * Common Usage
+ */
 const check = (reg, string) => reg.test(string);
 
-check(/\d+/g, 'test'); //false
-check(/[a-z]+/g, 'test'); //true
+check(/\d+/g, 'test'); // false
+check(/[a-z]+/g, 'test'); // true
 
+
+
+/**
+ * currying
+ */
 const curryingCheck = reg => string => reg.test(string);
 
 const hasNumber = curryingCheck(/\d+/g);
