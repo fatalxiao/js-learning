5d95a9d25c5962103d640c5bfe1070f152481b49
liangxiaojun <fatalxiao@163.com>
2019-09-11 17:06:38 +0800

    update LongestIncreaseSequence

--- src/containers/app/modules/DynamicProgramming/LongestIncreaseSequence/LongestIncreaseSequence.js
+++ src/containers/app/modules/DynamicProgramming/LongestIncreaseSequence/LongestIncreaseSequence.js
@@ -1,8 +1,11 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 import Code from 'components/Code/Code';
 
-import value from '!!raw-loader!modules/DynamicProgramming/LongestIncreaseSequence/LongestIncreaseSequence';
+import LongestIncreaseSequenceCode from '!!raw-loader!modules/DynamicProgramming/LongestIncreaseSequence/LongestIncreaseSequence';
+import LongestIncreaseSequenceRecursionCode from '!!raw-loader!modules/DynamicProgramming/LongestIncreaseSequence/LongestIncreaseSequenceRecursion';
+import LongestIncreaseSequenceRecursionAdvancedCode from '!!raw-loader!modules/DynamicProgramming/LongestIncreaseSequence/LongestIncreaseSequenceRecursionAdvanced';
+import LongestIncreaseSequenceRecursionAdvancedWithCacheCode from '!!raw-loader!modules/DynamicProgramming/LongestIncreaseSequence/LongestIncreaseSequenceRecursionAdvancedWithCache';
 
 class LongestIncreaseSequence extends Component {
 
@@ -12,7 +15,12 @@ class LongestIncreaseSequence extends Component {
 
     render() {
         return (
-            <Code value={value}/>
+            <Fragment>
+                <Code value={LongestIncreaseSequenceCode}/>
+                <Code value={LongestIncreaseSequenceRecursionCode}/>
+                <Code value={LongestIncreaseSequenceRecursionAdvancedCode}/>
+                <Code value={LongestIncreaseSequenceRecursionAdvancedWithCacheCode}/>
+            </Fragment>
         );
     }
 }
