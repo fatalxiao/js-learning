1850a4c90dfd0b533193d481076de1775c1d5815
liangxiaojun <fatalxiao@163.com>
2019-09-24 15:25:47 +0800

    update LongestIncreaseSequence

--- src/containers/app/modules/DynamicProgramming/LongestIncreaseSequence/LongestIncreaseSequence.js
+++ src/containers/app/modules/DynamicProgramming/LongestIncreaseSequence/LongestIncreaseSequence.js
@@ -16,14 +16,15 @@ class LongestIncreaseSequence extends Component {
     render() {
         return (
             <Fragment>
-                <Code title="1. 最长上升序列"
-                      value={LongestIncreaseSequenceCode}/>
-                <Code title="2. 最长上升序列递归实现"
+                <h1>最长上升序列</h1>
+                <Code title="1. 递归实现"
                       value={LongestIncreaseSequenceRecursionCode}/>
-                <Code title="3. 最长上升序列递归实现，改进版，传递索引"
+                <Code title="2. 递归实现，改进版，传递索引"
                       value={LongestIncreaseSequenceRecursionAdvancedCode}/>
-                <Code title="4. 最长上升序列递归实现，改进版，传递索引，并添加缓存"
+                <Code title="3. 递归实现，改进版，传递索引，并添加缓存"
                       value={LongestIncreaseSequenceRecursionAdvancedWithCacheCode}/>
+                <Code title="4. 动态规划实现"
+                      value={LongestIncreaseSequenceCode}/>
             </Fragment>
         );
     }
