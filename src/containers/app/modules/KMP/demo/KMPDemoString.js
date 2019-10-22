ef7915fe7017cb82f71cb4d4998c81d33697f5e3
liangxiaojun <fatalxiao@163.com>
2019-10-22 16:04:26 +0800

    update KMPDemoString

---
+++ src/containers/app/modules/KMP/demo/KMPDemoString.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import 'scss/containers/app/modules/KMP/demo/KMPDemoString.scss';
+
+class KMPDemoString extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        const {title, value, offset, isCharActivated} = this.props;
+
+        return value ?
+            <div className="kmp-demo-string">
+
+                <div className="kmp-demo-string-title">
+                    {title}
+                </div>
+
+                <div className="kmp-demo-string-content"
+                     style={{paddingLeft: offset * 64}}>
+                    {
+                        value.split('').map((char, index) =>
+                            <div key={index}
+                                 className={classNames('kmp-demo-char', {
+                                     activated: isCharActivated && isCharActivated(index)
+                                 })}>
+                                {char}
+                            </div>
+                        )
+                    }
+                </div>
+
+            </div>
+            :
+            null;
+
+    }
+}
+
+KMPDemoString.propTypes = {
+    title: PropTypes.string,
+    value: PropTypes.string,
+    offset: PropTypes.number,
+    isCharActivated: PropTypes.func
+};
+
+export default KMPDemoString;
