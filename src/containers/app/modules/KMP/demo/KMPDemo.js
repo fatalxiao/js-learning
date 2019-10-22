6c06b2830c4f5df7e63c23747e15bbc63d578152
liangxiaojun <fatalxiao@163.com>
2019-10-22 16:04:44 +0800

    update KMPDemo

--- src/containers/app/modules/KMP/demo/KMPDemo.js
+++ src/containers/app/modules/KMP/demo/KMPDemo.js
@@ -1,17 +1,62 @@
 import React, {Component, Fragment} from 'react';
 
+import RaisedButton from 'alcedo-ui/RaisedButton';
+import KMPDemoStringGroup from './KMPDemoStringGroup';
+
+import KMPDemoClass from 'modules/KMP/demo';
+
 class KMPDemo extends Component {
 
     constructor(props) {
+
         super(props);
+
+        this.txt = 'aaaaaaab';
+        this.pat = 'aaab';
+
+        this.kmp = new KMPDemoClass(this.pat);
+        this.step = this.kmp.search(this.txt);
+
+        this.state = {
+            currentState: null
+        };
+
     }
 
+    init = () => {
+        this.step = this.kmp.search(this.txt);
+        this.setState({
+            currentState: null
+        });
+    };
+
+    next = () => {
+        this.setState({
+            currentState: this.step.next()
+        });
+    };
+
     render() {
+
+        const {currentState} = this.state;
+
         return (
             <Fragment>
 
+                <KMPDemoStringGroup txt={this.txt}
+                                    pat={this.pat}
+                                    currentState={currentState && currentState.value}/>
+
+                <RaisedButton value="Reset"
+                              onClick={this.init}/>
+
+                <RaisedButton value="Next"
+                              disabled={currentState && currentState.value && currentState.value[1] === this.pat.length}
+                              onClick={this.next}/>
+
             </Fragment>
         );
+
     }
 }
 
